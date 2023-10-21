591a4ac6267c718e0b65f20b732eec5bb9013f3f
KethellynFreitas <kethellynsouzaf@gmail.com>
2023-10-21 17:50:19 -0300

    update ARMY rev.6

--- js/script_menu.js
+++ js/script_menu.js
@@ -1,20 +1,20 @@
 // Script responsável pela automoção do menu hamburger
 
-constMobile = document.getElementById('btn-mobile')
+const btnMobile = document.getElementById('btn-mobile');
 // const >> declara a nossa variável, no uso decorrer do códico 
 
 function toggleMenu(event) {
-    if (event.type === 'touchastart') event.preventDeFault();
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active')
+    if (event.type === 'touchstart') event.preventDeFault();
+    const nav = document.getElementById('nav');
+      nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active);
     if (active) {
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('arial-label', "Abrir Menu");
+      } else {
+        event.currentTarget.setAttribute('arial-label', 'Abrir Menu');
     }
 }
 
-btnMobile.addEventListener('click, toggleMenu')
-btnMobile;addEventListener('touchstart', toggleMenu)
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
